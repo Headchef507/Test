85e40b550ca3aaa1724fc00891d7625e05479b16
Rafno <rafnar1993@gmail.com>
2017-12-28 20:40:21 +0000

    rest breytt

--- Hæ.java
+++ Hæ.java
@@ -2,7 +2,6 @@
     public static void main(String[] args){
         int a = 5;
         String b = "leyni branch yolo";
-        System.out.println(a + "Hæ heimur" + " " + b);
         System.out.println(a + "Hæ heimur" + " " + b)
     }
 }
